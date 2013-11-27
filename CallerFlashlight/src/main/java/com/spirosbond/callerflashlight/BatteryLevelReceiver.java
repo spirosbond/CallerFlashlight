c2c81801f4357c71179622b5806f643ffead6b54
spiros <spirosbond@hotmail.com>
2013-11-28 00:59:14 +0200

    v3.0 Added translations Removed a toast on low battery Code optimization

--- CallerFlashlight/src/main/java/com/spirosbond/callerflashlight/BatteryLevelReceiver.java
+++ CallerFlashlight/src/main/java/com/spirosbond/callerflashlight/BatteryLevelReceiver.java
@@ -4,7 +4,6 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
-import android.widget.Toast;
 
 /**
  * Created by spiros on 11/7/13.
@@ -16,7 +15,7 @@ public class BatteryLevelReceiver extends BroadcastReceiver {
 	public void onReceive(Context context, Intent intent) {
 		CallerFlashlight callerFlashlight = (CallerFlashlight) context.getApplicationContext();
 		//		String level = BatteryManager.EXTRA_LEVEL;
-		if (CallerFlashlight.LOG) Toast.makeText(context, intent.getAction(), Toast.LENGTH_LONG).show();
+		//		if (CallerFlashlight.LOG) Toast.makeText(context, intent.getAction(), Toast.LENGTH_LONG).show();
 		if (CallerFlashlight.LOG) Log.d(TAG, "intent action: " + intent.getAction());
 		//		if (CallerFlashlight.LOG) Log.d(TAG, "battery level:" + level);
 		if (intent.getAction().equals(Intent.ACTION_BATTERY_LOW)) callerFlashlight.setLowBat(true);
